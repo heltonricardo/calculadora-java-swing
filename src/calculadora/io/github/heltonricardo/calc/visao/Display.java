c83d14123674bb2456ff679725e14432ae9bcf12
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-15 12:07:49 -0300

    Create Display.java

---
+++ src/calculadora/io/github/heltonricardo/calc/visao/Display.java
@@ -0,0 +1,10 @@
+package calculadora.io.github.heltonricardo.calc.visao;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Display extends JPanel {
+
+	public Display() {
+	}
+}
