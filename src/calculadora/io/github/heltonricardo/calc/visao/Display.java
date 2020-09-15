7b3c2b4342086b9018242442e461c57f2ab4437c
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-15 12:18:23 -0300

    Update Display.java

--- src/calculadora/io/github/heltonricardo/calc/visao/Display.java
+++ src/calculadora/io/github/heltonricardo/calc/visao/Display.java
@@ -1,10 +1,26 @@
 package calculadora.io.github.heltonricardo.calc.visao;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 
+	private final JLabel label;
+	
 	public Display() {
+		setBackground(new Color(46, 49, 50));
+		
+		label = new JLabel("1234,56");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 20));
+		
+		add(label);
 	}
 }
