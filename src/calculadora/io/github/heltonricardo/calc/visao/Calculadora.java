76d34367a9a890e431d0cb1721819e4cc065916f
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-15 12:07:45 -0300

    Update Calculadora.java

--- src/calculadora/io/github/heltonricardo/calc/visao/Calculadora.java
+++ src/calculadora/io/github/heltonricardo/calc/visao/Calculadora.java
@@ -1,17 +1,32 @@
 package calculadora.io.github.heltonricardo.calc.visao;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class Calculadora extends JFrame {
 
 	public Calculadora() {
+		organizarLayout();
+		
 		setVisible(true);
 		setSize(232, 322);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
+	private void organizarLayout() {
+		setLayout(new BorderLayout());
+		
+		Display display = new Display();
+		display.setPreferredSize(new Dimension(232, 60));
+		add(display, BorderLayout.NORTH);
+		
+		add(new Teclado(), BorderLayout.CENTER);
+	}
+
 	public static void main(String[] args) {
 		
 		new Calculadora();
