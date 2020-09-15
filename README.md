f37adcabedff4347bf13853d4c88da95aa6aaa35
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-15 10:29:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# calculadora
+ Calculadora implementada em Java usando Swing para interface gráfica com usuário.
---
+++ src/calculadora/io/github/heltonricardo/calc/visao/Calculadora.java
@@ -0,0 +1,19 @@
+package calculadora.io.github.heltonricardo.calc.visao;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame {
+
+	public Calculadora() {
+		setVisible(true);
+		setSize(232, 322);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		
+		new Calculadora();
+	}
+}
