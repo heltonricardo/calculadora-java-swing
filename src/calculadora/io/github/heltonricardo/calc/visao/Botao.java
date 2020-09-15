451b7121904770728902bdcff28bcc5612ebcf56
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-15 13:43:27 -0300

    Update Botao.java

--- src/calculadora/io/github/heltonricardo/calc/visao/Botao.java
+++ src/calculadora/io/github/heltonricardo/calc/visao/Botao.java
@@ -1,8 +1,20 @@
 package calculadora.io.github.heltonricardo.calc.visao;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton {
 
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setFont(new Font("courier", Font.PLAIN, 25));
+		setOpaque(true);
+		setForeground(Color.WHITE);
+		setBackground(cor);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
 }
