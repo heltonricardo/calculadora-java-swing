1c06fd5acd918589fd9adfd5225f386dd4656b00
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-16 12:14:00 -0300

    Update Memoria.java

--- src/calculadora/io/github/heltonricardo/calc/modelo/Memoria.java
+++ src/calculadora/io/github/heltonricardo/calc/modelo/Memoria.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Memoria {
-
+	
+	private enum TipoComando {
+		ZERAR, ALTER, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA
+	};
+	
 	private static final Memoria instancia = new Memoria();
 	private final List<MemoriaObservador> observadores = new ArrayList<>();
-	private String texto = "";
+	private String textoAtual = "";
 	
 	private Memoria() {}
 	
@@ -16,17 +20,45 @@ public class Memoria {
 	}
 	
 	public String getTexto() {
-		return texto.isEmpty() ? "0" : texto;
+		return textoAtual.isEmpty() ? "0" : textoAtual;
 	}
 	
 	public void registrarObservadores(MemoriaObservador obs) {
 		observadores.add(obs);
 	}
 	
-	public void processarValor(String valor) {
-		if (valor.equalsIgnoreCase("AC"))
-			texto = ""; 
-		else texto += valor;
+	private TipoComando detectarTipo(String textoBotao) {
+		if(textoAtual.isEmpty() && textoBotao == "0")
+			return null;
+		
+		try {
+			Integer.parseInt(textoBotao);
+			return TipoComando.NUMERO;
+		} catch (NumberFormatException e) {
+			if (textoBotao.equalsIgnoreCase("AC"))
+				return TipoComando.ZERAR;
+			if (textoBotao.equalsIgnoreCase("+"))
+				return TipoComando.SOMA;
+			if (textoBotao.equalsIgnoreCase("-"))
+				return TipoComando.SUB;
+			if (textoBotao.equalsIgnoreCase("×"))
+				return TipoComando.MULT;
+			if (textoBotao.equalsIgnoreCase("÷"))
+				return TipoComando.DIV;
+			if (textoBotao.equalsIgnoreCase(","))
+				return TipoComando.VIRGULA;
+			if (textoBotao.equalsIgnoreCase("±"))
+				return TipoComando.ALTER;
+			if (textoBotao.equalsIgnoreCase("="))
+				return TipoComando.IGUAL;
+		}
+		return null;
+	}
+	
+	public void processarValor(String textoBotao) {
+		TipoComando tipo = detectarTipo(textoBotao);
+		System.out.println(tipo);
+		textoAtual += textoBotao;
 		observadores.stream().forEach(o -> o.valorAlterado(getTexto()));
 	}
 }
