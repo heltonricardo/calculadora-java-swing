4ff3c3e6dad9a9addd9e315d64acfc4b78d632e5
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-16 11:04:19 -0300

    Update Memoria.java

--- src/calculadora/io/github/heltonricardo/calc/modelo/Memoria.java
+++ src/calculadora/io/github/heltonricardo/calc/modelo/Memoria.java
@@ -1,8 +1,12 @@
 package calculadora.io.github.heltonricardo.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
 
 	private static final Memoria instancia = new Memoria();
+	private final List<MemoriaObservador> observadores = new ArrayList<>();
 	private String texto = "";
 	
 	private Memoria() {}
@@ -13,5 +17,16 @@ public class Memoria {
 	
 	public String getTexto() {
 		return texto.isEmpty() ? "0" : texto;
+	}
+	
+	public void registrarObservadores(MemoriaObservador obs) {
+		observadores.add(obs);
+	}
+	
+	public void processarValor(String valor) {
+		if (valor.equalsIgnoreCase("AC"))
+			texto = ""; 
+		else texto += valor;
+		observadores.stream().forEach(o -> o.valorAlterado(getTexto()));
 	}
 }
